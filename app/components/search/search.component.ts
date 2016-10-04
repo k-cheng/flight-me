f9bf1fde367555d64910aa49bf83a62373cc8c02
Kevin Cheng <09chengk@gmail.com>
2016-10-04 13:13:31 -0700

    set up navbar skeleton

---
+++ app/components/search/search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId:module.id // lets us use relative pathing for templateUrl
+    selector: 'search',
+    templateUrl: 'search.component.html'
+})
+export class SearchComponent { }
